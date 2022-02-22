d449d3f62970a560fd37c3b87070226fd785b7ef
dyoder838 <dyoder838@gmail.com>
2022-02-22 12:06:41 -0800

    added routes to MainPage and DashBoard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from '../src/pages/Main';
+import DashBoard from '../src/pages/DashBoard';
 
 
 function App() {
   return (
-    <div>
-       <p>DeepSurface Project</p>
-    </div>
+    <Router>
+
+      <Routes>
+
+        <Route exact path="/" element={<MainPage />} />;
+        <Route exact path="/dashboard" element={<DashBoard />} />;
+        
+      </Routes>
+
+    </Router>
   );
 }
 
---
+++ src/pages/DashBoard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function DashBoard() {
+
+    return(
+        <div>
+
+            <p>DashBoard</p>
+
+        </div>
+    );
+
+};
